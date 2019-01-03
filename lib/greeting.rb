0faccf7603432df987a465bf69a5a8a44ceab02d
Dusto´n Lomrt <simon.m.f.w.knott@eclipso.de>
2019-01-03 14:22:18 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,2 +1,3 @@
 def greeting(name)
-  puts "Hello #{name}. It's nice to meet you."# code the #greeting method here!
+  puts "Hello #{name}. It's nice to meet you."
+end# code the #greeting method here!
